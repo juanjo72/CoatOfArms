48ac0455a1410d0d20dec5a0fad6737d106ea17e
Juanjo Garcia <juanvillaescusa72@gmail.com>
2024-08-11 12:35:15 +0200

    Initial Commit

---
+++ CoatOfArms/CoatOfArmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoatOfArmsApp.swift
+//  CoatOfArms
+//
+//  Created by Juanjo García Villaescusa on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct CoatOfArmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
